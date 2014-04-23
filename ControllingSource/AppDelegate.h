790c475b7174ec91a4e23edca7e7d6a853794d30
Dharmendra singh <Dharmendra.singh@zecross.com>
2014-04-23 18:28:16 +0530

    Initial Commit

---
+++ ControllingSource/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ControllingSource
+//
+//  Created by Dharmendra's iMac on 23/04/14.
+//  Copyright (c) 2014 Zecross Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ControllingSource/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ControllingSource
+//
+//  Created by Dharmendra's iMac on 23/04/14.
+//  Copyright (c) 2014 Zecross Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
